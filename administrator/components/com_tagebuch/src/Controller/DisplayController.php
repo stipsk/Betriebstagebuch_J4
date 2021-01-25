1ac5a61e84a5a613cc5a78b90f6b6681cfc787a1
Stephan Knauer <stephan.knauer@web.de>
2021-01-25 15:00:36 +0100

    Grundgerüst einfügen

---
+++ administrator/components/com_tagebuch/src/Controller/DisplayController.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * @package     Tagebuch.Administrator
+ * @subpackage  com_tagebuch
+ *
+ * @copyright   Copyright (C) 2021 Stephan Knauer. All rights reserved.
+ * @license     GNU/GPL Version 2 or later - http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+namespace SK\Component\Tagebuch\Administrator\Controller;
+
+defined('_JEXEC') or die;
+
+use Joomla\CMS\MVC\Controller\BaseController;
+
+/**
+ * Component Controller
+ *
+ * @since  1.5
+ */
+class DisplayController extends BaseController
+{
+	/**
+	 * The default view.
+	 *
+	 * @var    string
+	 * @since  1.6
+	 */
+	protected $default_view = 'tagebuch';
+
+	/**
+	 * Method to display a view.
+	 *
+	 * @param   boolean  $cachable   If true, the view output will be cached
+	 * @param   array    $urlparams  An array of safe URL parameters and their variable types, for valid values see {@link \JFilterInput::clean()}.
+	 *
+	 * @return  static  This object to support chaining.
+	 *
+	 * @since   1.5
+	 */
+	public function display($cachable = false, $urlparams = array())
+	{
+		return parent::display();
+	}
+}
