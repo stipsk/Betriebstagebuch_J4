088e190c81efedbaeb242768099dbf99ec6285b2
Stephan Knauer <stephan.knauer@web.de>
2021-01-25 10:07:46 +0100

    .gitignore angepasst und Configurationsdateien im Adminbereich erstellt

---
+++ administrator/components/com_tagebuch/sql/uninstall.mysql.utf8.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS `#__sk_tagebuch`;
+<<<<<<< HEAD
+
+DROP TABLE IF EXISTS `#__sk_tagebuch_avv`;
+
+DROP TABLE IF EXISTS `#__sk_tagebuch_bereich`;
+=======
+DROP TABLE IF EXISTS `#__sk_tagebuch_avv`;
+DROP TABLE IF EXISTS `#__sk_tagebuch_luft`;
+DROP TABLE IF EXISTS `#__sk_tagebuch_wasser`;
+>>>>>>> origin/1.0.0.0
